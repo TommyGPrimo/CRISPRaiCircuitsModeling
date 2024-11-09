bc0b077f829d3884389515bacdfc051e0150a912
Tommy G. Primo <tommygprimo@gmail.com>
2024-11-08 16:14:30 -0800

    Basic logging and init template

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,16 @@
-print('hello world')
+"""__init__ file: initializer should contain all necessary information to run package
+"""
+
+# Import functions
+import logging
+
+# List the names of all modules used for working with package, version
+__all__ = ["crisprai_model_test"]
+__version__ = "1.0.0"
+__name__ = "CRISPRai_Genetic_Circuits_Model"
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+print("Run Sucessfully!")
