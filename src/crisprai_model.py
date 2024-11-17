72e4a48a19c9ec63c387d3ad3dbc0642133c1a93
Tommy G. Primo <tommygprimo@gmail.com>
2024-11-17 13:12:24 -0800

    Changes

--- src/crisprai_model.py
+++ src/crisprai_model.py
@@ -10,25 +10,45 @@ import logging
 class crisprai_model:
     
     def checkGeneticCircuitComponents():
-        """checkGeneticCircuitComponents
+        """ checkGeneticCircuitComponents
         - Prints out the values of the CRISPRai components including genetic circuit components for review by the user
         """
-        pass
+        print("Checking Genetic circuit components")
 
     def simulateGeneticCircuits():
-        pass
+        """ simulateGeneticCircuits
+        - Generates a tellurium simulation of the steady state response of the input
+        """
+        print("Simulating genetic circuits")
 
-    def changeMachineryConcentration(dCas9=1, Activator=1):
-        pass
+    def changeMachineryConcentration(dCas9=1, Activator=1, *args):
+        """ changeMachinery Concentration
+        - Changes the dCas9 and MCP-SoxS activator machinery
+        Args:
+            dCas9 (int, optional): dCas9 protein concentration. Defaults to 1.
+            Activator (int, optional): MCP-SoxS protein complex concentration. Defaults to 1.
+            args: Array for K values that influence machinery concentration
+        """
+        print("Changing machinery concentration")
 
     def setCompetitorCircuits(value=1):
-        pass
+        """setCompetitorCircuits
+        - This function changes the tellurium model depending on the value supplied to increase the number of competitors
+        Args:
+            value (int, optional): _description_. Defaults to 1.
+        """
+        print("Set competitor circuits")
 
     def setTargetCircuit(value=1):
-        pass
+        """SetTargetCircuit, Define the target DNA you're targeting, this should change the parameter of DNA being targeted
+
+        Args:
+            value (int, optional): _description_. Defaults to 1.
+        """
+        print("Define target circuit")
 
     def changeInputResponseRange(responseRange):
-        pass
+        print("Changing the input response range")
 
     def extractSteadyStateValues():
-        pass
+        print("Get steady state")
