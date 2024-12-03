78fbe13c7f3cf4163d0e9c8675516d782d42345f
Tommy G. Primo <tommygprimo@gmail.com>
2024-12-03 15:16:35 -0800

    A lot of changes

--- crisprai_model/crisprai_model.py
+++ crisprai_model/crisprai_model.py
@@ -8,7 +8,10 @@ import matplotlib.pyplot as plt
 
 class crisprai_model:
     
-    def create_model(self):
+    def get_crispra_model(self):
+        """
+        Returns the crispra model function and plots the model
+        """
         # Define CRISPRa modality two model - scRNA recruited activator protein complex coupled with dCas9
         model = """
         # CRISPRa Modality 1 - dCas9 tethered to activator model
@@ -39,32 +42,16 @@ class crisprai_model:
         del = 1;
         """
         return model
-        
-    def change_machinery(crispr_complex=1, target_gene=1):
-        
-        """ChangeCRISPRMachineryConcentration - This function allows user to change the concentration of the CRISPRa machinery or target gene
-
-        Args:
-            crispr_complex (int, optional): dCas9-Activator tethered complex concentration. Defaults to 1.
-            target_gene (int, optional): the target gene for your desired gene activation. Defaults to 1.
-        """
-        print("Machinery Changed")
-
-    def simulate_crispra_model(model, time=10):
-        """ simulateGeneticCircuits
-        - Generates a tellurium simulation of the steady state response of the input
-        Args:
-            model (tellurium): The tellurium model needed to simulate the plot
-            time (int): The time to measure concentrations
-        """
-        rr = te.loada(model)
-        model_data = rr.simulate(0,time,200)
-        rr.plot(title="CRISPRa Modality I Model", xlabel= "Time (sec)", ylabel= "Concentration")
-        return model_data
-
-    def define_io_model():
-        
-        # Reduced ODE model
+    
+    def simulate_crispra_model(self, model, time):
+        m = te.loada(model)
+        mm = m.simulate(0,time,2000)
+        return [m,mm]
+    
+    def plot_crispra_model(self,model):
+        model.plot(title="CRISPRa tethered Model", xlabel= "Time (sec)", ylabel= "Concentration")
+    
+    def get_io_model(self):
         reduced_model = """
         # Reactions
         R0: -> s1; u1-del*s1;
@@ -80,7 +67,7 @@ class crisprai_model:
 
         del = 1;
         gam = 1;
-
+        
         kap = 1000;
         D = 10;
         D2 = 10;
@@ -88,46 +75,64 @@ class crisprai_model:
         K_l = 3;
         d = 100;
         """
-        
-            # Initialize reduced tellurium model
-        rtm = te.loada(reduced_model)
+        return reduced_model
+    
+    # Simulate the IO model
+    def simulate_io_model(self, reduced_model):
+        rm = te.loada(reduced_model)
         rm_data = rm.simulate(0,1,2000)
-        
-        # If plotting is needed - uncomment
-        #rtm.plot(title="CRISPRa Modality I Model", xlabel= "Time (sec)", ylabel= "Concentration")
-        return rtm
-
-    def simulate_io_response(rm):
-        # Reset the tellurium model
-        rm.reset()
-        
-        print("Simulate IO")
-
-    # Define function to change steady
-    def change_steadystate_values(tellurium_model, inducer_concentrations, steady_state_protein_list):
+        return [rm,rm_data]
+    
+    def plot_io_model(self,io):
+        io.plot(title="CRISPRa tethered Model", xlabel= "Time (sec)", ylabel= "Concentration")
+    
+    # Define function to change steady state values with changing inducer for io plotting
+    def change_steady_state_values(self,tellurium_model, inducer_concentrations, steady_state_protein_list):
         for inducer in inducer_concentrations:
             tellurium_model.u1 = inducer
             simulated_model = tellurium_model.simulate()
-            simulated_steady_state_protein = simulated_model['[Y]'][-1]
-            steady_state_protein_list.append(simulated_steady_state_protein)
-            
-    def plot_io_function(input, steady_state_protein_concentrations,color = 'black'):
-        plt.plot(input, steady_state_protein_concentrations, color)
-
-    def define_io_plot(label):
-        """define_io_plot - Function sets up plotting function for the io model
+            simulated_steady_state_proteins = simulated_model['[Y]'][-1]
+            steady_state_protein_list.append(simulated_steady_state_proteins)
+    
+    def visualize_io_response(self, rm_model, inducer_range, complex_comparisons):
+        # rest the reduced model
+        rm_model.reset()
         
-            Args:
-                label (Array,String): Array of string comparing labels
-        """
-        # Plot input to output
+        # Define varying inducer values
+        inducer_values = np.logspace(-inducer_range,inducer_range, 100)
+        
+        # Define empty steadyStateProteinsList for two conditions
+        steady_state_condition_1 = []
+        steady_state_condition_2 = []
+        
+        # Change complex concentration and populate steady state concentrations
+        rm_model.d = complex_comparisons[0]
+        self.change_steady_state_values(rm_model, inducer_values, steady_state_condition_1)
+        
+        # Rest model
+        rm_model.reset()
+        
+        # Change complex concentrations and populate steady state concentrations for secondary comparison
+        rm_model.d = complex_comparisons[1]
+        self.change_steady_state_values(rm_model, inducer_values, steady_state_condition_2)
+        
+        # Nested function for plotting io response
+        def plot_io(input, ss_protein_concentrations,color = 'black'):
+            plt.plot(input, ss_protein_concentrations, color)
+        
+        # Plotting the io responses
+        plot_io(inducer_values, steady_state_condition_1)
+        plot_io(inducer_values, steady_state_condition_2, 'red')
+        
+        # Setting up the plot descriptions and information
         plt.xlabel('$inducer_i$ ')
         plt.ylabel('$Protein_i$')
         plt.xscale('log')
         plt.title('CRISPRa I/O Response')
 
-        red_patch = plt.Line2D([0], [0], color='red', lw=2, label=label[0])
-        grey_patch = plt.Line2D([0], [0], color='black', lw=2, label=label[1])
-        
+        red_patch = plt.Line2D([0], [0], color='red', lw=2, label=f'complex={complex_comparisons[1]}')
+        grey_patch = plt.Line2D([0], [0], color='black', lw=2, label=f'complex={complex_comparisons[0]}')
         plt.legend(handles=[red_patch, grey_patch], loc='lower right', fontsize=11); # Semi-colon to remove print statement 
         plt.show()
+        
+        
---
+++ crisprai_model/crisprai_model_test.py
@@ -0,0 +1,21 @@
+""" CRISPRai_model testing file
+"""
+
+# Imports
+import unittest
+from crisprai_model import crisprai_model as cai
+
+# Initialize the package and model
+cai_circuit = cai()
+cai_model = cai_circuit.get_crispra_model() # Test this by making sure you get a string returned
+
+# Plot model
+cai_circuit.simulate_crispra_model(cai_model, time=20) # Test this to see if you're getting the array dynamics for your model...can adjust the time
+
+# IO visualization
+io_model = cai_circuit.get_io_model()
+io_simulation = cai_circuit.simulate_io_model(io_model)
+
+cai_circuit.visualize_io_response(io_simulation[0], 8, [10,10000]) # Comparing CRISPRa complex and 10x CRISPRa complex, for how much inducer is needed``
+
+
